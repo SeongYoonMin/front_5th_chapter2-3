5d967aff3f91c20340060cd15e646d0c9f5c2a20
SeongYoonMin <namo4862@naver.com>
2025-05-01 23:47:12 +0900

    remove: API, model 구조 변경으로 인한 제거

--- src/features/postList/api/getPostList.ts
+++
@@ -1,5 +0,0 @@
-import { customAxios } from "@/shared/api/customAxios";
-
-export const getPostList = async ({ limit, skip }: { limit: number; skip: number; }) => {
-  return customAxios.get(`/api/posts`, { params: { limit, skip } })
-}
