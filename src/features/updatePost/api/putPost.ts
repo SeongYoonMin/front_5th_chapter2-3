cefa690b0f3188d9a8f555a66e0fe2036d3459f1
SeongYoonMin <namo4862@naver.com>
2025-05-02 04:45:40 +0900

    feat: putPost API 추가

---
+++ src/features/updatePost/api/putPost.ts
@@ -0,0 +1,9 @@
+import { IPostItem } from "@/entities/posts/model/post.types";
+import { customAxios } from "@/shared/api/customAxios";
+
+export const putPost = async ({ post }: { post: IPostItem }) => {
+  const { data } = await customAxios.put(`/api/posts/${post.id}`, {
+    body: post,
+  })
+  return data
+}
