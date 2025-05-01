823fdc3323cdd173455551eb7a58c70c291966ed
SeongYoonMin <namo4862@naver.com>
2025-05-02 04:39:09 +0900

    feat: post제거 구현

--- src/features/deletePost/ui/DeletePost.tsx
+++ src/features/deletePost/ui/DeletePost.tsx
@@ -1,11 +1,16 @@
 import { usePostStore } from '@/entities/posts/model/postStore'
 import { Button } from '@/shared/ui'
 import { Trash2 } from 'lucide-react'
+import { useDeletePost } from '../hooks/useDeletePost';
 
 const DeletePost = ({ postId }: { postId: number }) => {
-  const postStore = usePostStore()
+  const { removePost } = usePostStore();
+  const mutationDeletePost = useDeletePost();
+  const onClickDeletePost = () => {
+    mutationDeletePost.mutate({ id: postId });
+  }
   return (
-    <Button variant="ghost" size="sm" onClick={() => postStore.getState().removePost(postId)}>
+    <Button variant="ghost" size="sm" onClick={onClickDeletePost}>
       <Trash2 className="w-4 h-4" />
     </Button>
   )
