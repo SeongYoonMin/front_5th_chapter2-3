aee00ed7c06985a5fc3a1c080e6993f9da9a9b99
SeongYoonMin <namo4862@naver.com>
2025-05-02 04:37:11 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerLegacy />
+          {/* <PostsManagerLegacy /> */}
           <PostsManager />
         </main>
         <Footer />
