5edbbd03ef1bcac0d37b1f7991d32ac1eef45b4e
SeongYoon <namo4862@gmail.com>
2025-05-06 17:19:21 +0900

    fix: 토글부분 수정

--- src/features/postHeader/ui/PostHeader.tsx
+++ src/features/postHeader/ui/PostHeader.tsx
@@ -1,18 +1,20 @@
-import { CardHeader, CardTitle, Button } from '@/shared/ui'
-import { Plus } from 'lucide-react'
+import { usePostDialogStore } from "@/entities/posts/model/postDialogStore";
+import { CardHeader, CardTitle, Button } from "@/shared/ui";
+import { Plus } from "lucide-react";
 
 const PostHeader = () => {
+  const { toggleAddPostDialog } = usePostDialogStore();
   return (
     <CardHeader>
       <CardTitle className="flex items-center justify-between">
         <span>게시물 관리자</span>
-        <Button>
+        <Button onClick={toggleAddPostDialog}>
           <Plus className="w-4 h-4 mr-2" />
           게시물 추가
         </Button>
       </CardTitle>
     </CardHeader>
-  )
-}
+  );
+};
 
-export default PostHeader
+export default PostHeader;
