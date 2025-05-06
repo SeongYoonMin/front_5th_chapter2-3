45818e07455e7ba5cb6ef5c5c548fd279868219d
SeongYoon <namo4862@gmail.com>
2025-05-06 17:16:57 +0900

    fix: queryClient로 invalidateQueries(갱신) 호출

--- src/features/addComment/model/useAddComment.ts
+++ src/features/addComment/model/useAddComment.ts
@@ -1,14 +1,15 @@
-import { useMutation } from "@tanstack/react-query"
-import { postAddComment } from "../api/postAddComment"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { postAddComment } from "../api/postAddComment";
 
 import { IResponseAddComment } from "@/entities/comments/model/comment.types";
 export const useAddComment = () => {
-
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["addComment"],
     mutationFn: postAddComment,
     onSuccess: (data: IResponseAddComment) => {
-      return data
-    }
-  })
-}
+      queryClient.invalidateQueries({ queryKey: ["getComments", data.postId] });
+      return data;
+    },
+  });
+};
