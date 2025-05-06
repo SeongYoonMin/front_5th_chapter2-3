70980b5aa5f34617fbf18d2a4b98bdbc4ce37e56
SeongYoon <namo4862@gmail.com>
2025-05-06 17:10:05 +0900

    remove: 위치변경, 제거 등

--- src/entities/comments/hooks/useGetComments.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query"
-import { getComments } from "../api/getComments"
-
-export const useGetComments = ({ postId }: { postId: number }) => {
-  return useQuery({
-    queryKey: ["getComments", postId],
-    queryFn: async () => await getComments({ postId }),
-  })
-}
