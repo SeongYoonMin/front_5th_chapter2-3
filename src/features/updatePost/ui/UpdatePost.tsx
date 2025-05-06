e5f1479caf78d8166a222fabaf37fd07248ab327
SeongYoon <namo4862@gmail.com>
2025-05-06 17:20:34 +0900

    fix: 토글방식 수정

--- src/features/updatePost/ui/UpdatePost.tsx
+++ src/features/updatePost/ui/UpdatePost.tsx
@@ -1,22 +1,21 @@
-import { IPostItem } from '@/entities/posts/model/post.types'
-import { Button } from '@/shared/ui'
-import { Edit2 } from 'lucide-react'
+import { IPostItem } from "@/entities/posts/model/post.types";
+import { usePostDialogStore } from "@/entities/posts/model/postDialogStore";
+import { useSelectedStore } from "@/shared/model/selectStore";
+import { Button } from "@/shared/ui";
+import { Edit2 } from "lucide-react";
 
 const UpdatePost = ({ post }: { post: IPostItem }) => {
+  const { setSelectedPost } = useSelectedStore();
+  const { toggleUpdatePostDialog } = usePostDialogStore();
   const onClickUpdatePost = () => {
-    // setSelectedPost(post)
-    // setShowEditDialog(true)
-    console.log(post)
-  }
+    setSelectedPost(post);
+    toggleUpdatePostDialog();
+  };
   return (
-    <Button
-      variant="ghost"
-      size="sm"
-      onClick={onClickUpdatePost}
-    >
+    <Button variant="ghost" size="sm" onClick={onClickUpdatePost}>
       <Edit2 className="w-4 h-4" />
     </Button>
-  )
-}
+  );
+};
 
-export default UpdatePost
+export default UpdatePost;
